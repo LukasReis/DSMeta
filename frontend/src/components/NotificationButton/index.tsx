bc38b3a07bc8fb0b3d96c6d67ef307f802983b41
LukasReis <luke.silvareis@gmail.com>
2022-09-08 10:17:05 -0300

    Botao componente

---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,13 @@
+import iconNotification from '../../assets/img/notification-icon.svg'
+import  './styles.css'
+
+function NotificationButton() {
+    return(
+        <div className="dsmeta-red-btn">
+            <img src={iconNotification} alt="Notificar" />
+        </div>
+)
+}
+
+export default NotificationButton
+           
